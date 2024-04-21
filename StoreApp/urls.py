8bd07402df38875fc077b631f243f4700224f169
NaitikKaythwal <naitikkaythwal@gmail.com>
2024-04-21 01:10:08 -0500

    Implemented Product model, views, templates, and URL patterns

--- StoreApp/urls.py
+++ StoreApp/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
+from django.shortcuts import redirect
+from myapp import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('', lambda request: redirect('index')),  # Redirect root URL to products index
+    path('products/', views.index, name='index'),
+    path('products/<int:pk>/', views.show, name='show'),
 ]
+
+
--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+# views.py
+from django.shortcuts import render, get_object_or_404
+from .models import Product
 
-# Create your views here.
+def index(request):
+    products = Product.objects.all()
+    return render(request, 'myapp/index.html', {'products': products})
+
+def show(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'myapp/show.html', {'product': product})
